8b497f03f5df0273c591cd210f38ef0b710848b6
denilgabani <gabanidenil@gmail.com>
2021-11-22 11:31:23 +0530

    Added tests related to invalid input for signup

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -1,7 +1,7 @@
 import request from "supertest";
 import { app } from "../../app";
 
-it("will send 201 with successful user creation in db", () => {
+it("returns 201 with successful user creation in db", () => {
   return request(app)
     .post("/api/users/signup")
     .send({
@@ -9,4 +9,42 @@ it("will send 201 with successful user creation in db", () => {
       password: "123456",
     })
     .expect(201);
+});
+
+it("returns 400 when email is entered wrong", () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test",
+      password: "123456",
+    })
+    .expect(400);
+});
+
+it("returns 400 when password is not in current length", () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "123",
+    })
+    .expect(400);
+});
+
+it("returns 400 when email or password or both is not provided", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+    })
+    .expect(400);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      password: "123456",
+    })
+    .expect(400);
+
+  return request(app).post("/api/users/signup").send({}).expect(400);
 });
