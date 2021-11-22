fd0c011b4f089411e85f6f4612ba2982d59d5e4b
denilgabani <gabanidenil@gmail.com>
2021-11-22 11:44:33 +0530

    Added test to check if response from server has cookie after signup so we know that jwt has been set in cookie, but here when we make request from jest it will be http not https and we defined secure: true for cookieSession so cookie will be sent only if request made through https. So we have made it false if and only if request made while we are in 'test' envrionment we will sent cookie even if request made through http.

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -66,3 +66,15 @@ it("returns 400 when account is already registered with provided email", async (
     })
     .expect(400);
 });
+
+it("sets a cookie after successful signup", async () => {
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "123456",
+    })
+    .expect(201);
+
+  expect(response.get("Set-Cookie")).toBeDefined();
+});
