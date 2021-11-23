5be84f7c52060adfe3886432836f9b335b02aaac
denilgabani <gabanidenil@gmail.com>
2021-11-23 12:52:56 +0530

    Now added getInititalProps and identified when it will execute on server and browser if it's executed from server then we have to make request to ingress-nginx client is in cluster and can not make to request to auth server with base domain directly and in browser when request has been made it automatically intercept by ingress-nginx

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,23 @@
-const Index = () => {
+import axios from "axios";
+
+const LandingPage = () => {
+  console.log("In browser");
   return <h1>Landing Page</h1>;
 };
 
-export default Index;
+LandingPage.getInitialProps = async () => {
+  if (typeof window === "undefined") {
+    // In server. window only exist for browser.
+    // requests should be made to "http://ingress-nginx...."
+    // It happens when url entered, refreshed, clicking link from different domain
+    console.log("In server");
+  } else {
+    //   In Browser.
+    // Request should be made with base url
+    // Only happens when navigate to this from inside of app
+  }
+
+  return {};
+};
+
+export default LandingPage;
