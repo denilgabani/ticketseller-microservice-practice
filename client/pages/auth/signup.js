00ae26c4620c9ca13dbf83f43c5a5f8ba23f84da
denilgabani <gabanidenil@gmail.com>
2021-11-23 11:15:58 +0530

    Linked client signup to auth /api/users/signup api

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -4,16 +4,21 @@ import { useState } from "react";
 const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
 
   const onSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await axios.post("/api/users/signup", {
-      email,
-      password,
-    });
+    try {
+      const response = await axios.post("/api/users/signup", {
+        email,
+        password,
+      });
 
-    console.log(response.data);
+      console.log(response.data);
+    } catch (err) {
+      setErrors(err.response.data.errors);
+    }
   };
 
   return (
@@ -39,6 +44,17 @@ const SignUp = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
       </div>
+      {errors.length > 0 && (
+        <div className="alert alert-danger">
+          <h4>Invalid inputs...</h4>
+          <ul>
+            {errors.map((err) => (
+              <li key={err.error}>{err.error}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
