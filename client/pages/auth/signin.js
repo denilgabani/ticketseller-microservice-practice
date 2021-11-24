a139fa20fd7a83e3a98fe6af18f6120462658e78
denilgabani <gabanidenil@gmail.com>
2021-11-24 09:32:31 +0530

    Added the signin functionality same as signup

---
+++ client/pages/auth/signin.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+import { useState } from "react";
+import { useRequest } from "../../hooks/useRequest";
+import Router from "next/router";
+
+const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { doRequest, errors } = useRequest({
+    url: "/api/users/signin",
+    method: "post",
+    body: {
+      email,
+      password,
+    },
+    onSuccess: () => Router.push("/"),
+  });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    doRequest();
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>SignIn</h1>
+      <div className="form-group">
+        <label for="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          className="form-control"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label for="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          className="form-control"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      {errors}
+
+      <button className="btn btn-primary">Sign In</button>
+    </form>
+  );
+};
+
+export default SignIn;
--- client/pages/demo.js
+++
@@ -1,5 +0,0 @@
-const Demo = () => {
-  return <h1>Demo Page!!!!</h1>;
-};
-
-export default Demo;
