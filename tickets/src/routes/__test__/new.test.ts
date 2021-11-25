30101b0d4895deca43999f5f185b34d95ba9ec19
denilgabani <gabanidenil@gmail.com>
2021-11-25 11:26:01 +0530

    Added fake auth by generating fake cookie for testing purpose in tickets module so we don't have to depend on auth servicefor testing purpose

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,18 +1,25 @@
 import request from "supertest";
 import { app } from "../../app";
+import { signIn } from "../../test/signInHelper";
 
 it("must handle router /api/tickets/new", async () => {
   const response = await request(app).post("/api/tickets/new").send({});
 
   expect(response.statusCode).not.toEqual(404);
 });
+
 it("can only allow to access if user is signed in", async () => {
   await request(app).post("/api/tickets/new").send({}).expect(401);
 });
+
 it("return other than 401 if user is signed in", async () => {
-  const response = await request(app).post("/api/tickets/new").send({});
+  const response = await request(app)
+    .post("/api/tickets/new")
+    .set("Cookie", signIn())
+    .send({});
   expect(response.statusCode).not.toEqual(401);
 });
+
 it("returns an error if invalid title is provided", async () => {});
 it("returns an error if invalid price is provided", async () => {});
 it("creates a ticket with valid inputs", async () => {});
--- tickets/src/test/signInHelper.ts
+++ tickets/src/test/signInHelper.ts
@@ -1,15 +1,27 @@
 import request from "supertest";
 import { app } from "../app";
+import jwt from "jsonwebtoken";
 
-export const signIn = async () => {
-  const email = "test@test.com";
-  const password = "123456";
-  const response = await request(app).post("/api/users/signup").send({
-    email,
-    password,
-  });
+export const signIn = () => {
+  // Define a payload for jwt
+  const payload = {
+    id: "619f21a3a0651c3e94c4e985",
+    email: "test@test.com",
+  };
 
-  const cookie = response.get("Set-Cookie");
+  // Create the JWT
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
 
-  return cookie;
+  // Build session Object
+  const session = { jwt: token };
+
+  // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  // Take JSON and encode into base64
+  const base64Token = Buffer.from(sessionJSON).toString("base64");
+
+  // return string which is cookie with encoded data
+
+  return [`express:sess=${base64Token}`];
 };
