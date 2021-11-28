84214442a1897f7cd70154b31ce6a2d04a4ed934
denilgabani <gabanidenil@gmail.com>
2021-11-28 11:50:07 +0530

    Added NATS graceful shutdown in natConnect.ts

--- tickets/src/config/natsConnect.ts
+++ tickets/src/config/natsConnect.ts
@@ -3,6 +3,14 @@ import { natsWrapper } from "../NatsWrapper";
 const natsConnect = async () => {
   try {
     await natsWrapper.connect("ticketseller", "asdf", "http://nats-srv:4222");
+
+    natsWrapper.client.on("close", () => {
+      console.log("Connection to NATS closed");
+      process.exit();
+    });
+
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
   } catch (err) {
     console.error(err);
   }
