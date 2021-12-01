bcd4ff6bd7fdac5302588e8f719edb4b759b0e5a
denilgabani <gabanidenil@gmail.com>
2021-12-01 08:18:11 +0530

    Created TicketUpdatedListener

---
+++ orders/src/events/listeners/TicketUpdatedListener.ts
@@ -0,0 +1,25 @@
+import { Listener, Subjects, TicketUpdatedEvent } from "@dgticketseller/common";
+import { Message } from "node-nats-streaming";
+import { Ticket } from "../../models/Ticket";
+import { queueGroupName } from "./queueGroupName";
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+
+  queueGroupName: string = queueGroupName;
+
+  async onMessage(data: TicketUpdatedEvent["data"], msg: Message) {
+    const ticket = await Ticket.findById(data.id);
+
+    if (!ticket) {
+      throw new Error("Ticket Not Found");
+    }
+
+    const { title, price } = ticket;
+
+    ticket.set({ title, price });
+    await ticket.save();
+
+    msg.ack();
+  }
+}
