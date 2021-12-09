d5aac5073c1ee5d7b1fe7be5de78f757d14e0b73
denilgabani <gabanidenil@gmail.com>
2021-12-09 12:50:35 +0530

    Implemented ExpirationCompleteListener in orders service which will listened to event expiration:complete

---
+++ orders/src/events/listeners/ExpirationCompleteListener.ts
@@ -0,0 +1,42 @@
+import {
+  ExpirationCompleteEvent,
+  Listener,
+  NotFoundError,
+  OrderStatus,
+  Subjects,
+} from "@dgticketseller/common";
+import { Message } from "node-nats-streaming";
+import { Order } from "../../models/Order";
+import { natsWrapper } from "../../NatsWrapper";
+import { OrderCancelledPublisher } from "../publishers/OrderCancelledPublisher";
+import { queueGroupName } from "./queueGroupName";
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+  subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+  queueGroupName: string = queueGroupName;
+
+  async onMessage(data: ExpirationCompleteEvent["data"], msg: Message) {
+    const order = await Order.findById(data.orderId).populate("ticket");
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    order.set({
+      status: OrderStatus.Cancelled,
+    });
+
+    await order.save();
+
+    // Publishing the OrderCancelled event
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
+
+    msg.ack();
+  }
+}
