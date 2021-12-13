895753263bc5751b67a25e796213f5d98a76d3b9
denilgabani <gabanidenil@gmail.com>
2021-12-13 09:45:41 +0530

    Added functionality of stripe checkout

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,8 +1,18 @@
 import { useEffect, useState } from "react";
 import buildClient from "../../api/buildClient";
+import StripeCheckout from "react-stripe-checkout";
+import { useRequest } from "../../hooks/useRequest";
 
-const OrderShow = ({ order }) => {
+const OrderShow = ({ order, currentUser }) => {
   const [timeLeft, setTimeLeft] = useState(0);
+  const { doRequest, errors } = useRequest({
+    url: "/api/payments",
+    method: "post",
+    body: {
+      orderId: order.id,
+    },
+    onSuccess: (payments) => console.log(payments),
+  });
 
   useEffect(() => {
     const findTimeLeft = () => {
@@ -23,7 +33,20 @@ const OrderShow = ({ order }) => {
     return <div>Order is expired please create another order. Thanks!</div>;
   }
 
-  return <div>{timeLeft} seconds left to pay for the order</div>;
+  return (
+    <>
+      <div>
+        <p>{timeLeft} seconds left to pay for the order</p>
+        <br />
+        <StripeCheckout
+          stripeKey="pk_test_51K5DqNSJ4825hr5WFniUfD10Aep1O8Q7yauDVIlL9hJvCJAQTFSVRJC8VsrA50k0kjT4dUg1q5vBiQpVl00zxyTG00TuXRakWg"
+          token={({ id }) => doRequest({ token: id })}
+          amount={order.price * 100}
+          email={currentUser.email}
+        />
+      </div>
+    </>
+  );
 };
 
 export async function getServerSideProps(context) {
